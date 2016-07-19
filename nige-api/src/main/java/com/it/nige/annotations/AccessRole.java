3cc4f4599c19caa8cba5d02885a2546854f0dae4
geine <bonos2@wisdo.me>
2016-07-19 17:34:17 +0900

    AccessRole annotation

---
+++ nige-api/src/main/java/com/it/nige/annotations/AccessRole.java
@@ -0,0 +1,38 @@
+package com.it.nige.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * @author geine
+ *
+ */
+@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.CLASS)
+@Documented
+public @interface AccessRole {
+
+	Role role();
+	
+	enum Role {
+		
+		/**
+		 *  본인
+		 */
+		Author,
+		
+		/**
+		 *  상관 없음
+		 */
+		All,
+		
+		/**
+		 *  비 로그인 0,로그인 시 개인식별번호
+		 */
+		Selector
+	}
+}
